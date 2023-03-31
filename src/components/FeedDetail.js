eeebaa70d1bba70ad9e294d2a8a46855c266828d
Jose Ignacio Rodriguez <josei.roodriguez@gmail.com>
2023-03-31 13:21:39 +0200

    vista detalle integrada

--- src/components/FeedDetail.js
+++ src/components/FeedDetail.js
@@ -1,59 +1,68 @@
-import React, { useState, useRef } from 'react';
+/*Vista detalle de Feed que se pueda editar y borrar */
+import React, { useRef } from "react";
+import "./FeedDetail.css";
 
-function FeedDetail() {
-  const [feed, setFeed] = useState({});
-
+function FeedDetail({ handleCloseModal, editNew }) {
   const titleInputRef = useRef();
   const bodyInputRef = useRef();
   const imageInputRef = useRef();
   const sourceInputRef = useRef();
   const publisherInputRef = useRef();
 
+  const sendData = (feedBody) => {
+    editNew(feedBody);
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    setFeed({
-      title: titleInputRef,
-      content: bodyInputRef,
-      urlToImage: imageInputRef,
-      source: {name:sourceInputRef},
-      author: publisherInputRef
-    })
+    console.log(titleInputRef.current.value);
+    const feedBody = {
+      title: titleInputRef.current.value,
+      content: bodyInputRef.current.value,
+      urlToImage: imageInputRef.current.value,
+      source: { name: sourceInputRef.current.value },
+      author: publisherInputRef.current.value,
+    };
+    sendData(feedBody);
+    handleCloseModal();
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit} className="login-form">
-      <h1>Edit News</h1>
-      <label htmlFor="title">
-        Title:
-        <input type="text" id="title" ref={titleInputRef} />
-      </label>
-      <br />
-      <label htmlFor="body">
-        Body:
-        <input type="text" id="body" ref={bodyInputRef} />
-      </label>
-      <br />
-      <label htmlFor="image">
-        Image:
-        <input type="img" id="image" ref={imageInputRef} />
-      </label>
-      <br />
-      <label htmlFor="source">
-        Source:
-        <input type="text" id="source" ref={sourceInputRef} />
-      </label>
-      <br />
-      <label htmlFor="publisher">
-        Publisher:
-        <input type="text" id="publisher" ref={publisherInputRef} />
-      </label>
-      <br />
-      <button type="submit">Submit</button>
-    </form>
+    <div className="feed-detail-content">
+      <form className="feed-detail-form" onSubmit={handleSubmit}>
+        <h1 className="feed-detail-title">Edit News</h1>
+        <label htmlFor="title">
+          Title:
+          <input type="text" id="title" ref={titleInputRef} />
+        </label>
+        <br />
+        <label htmlFor="body">
+          Body:
+          <input type="text" id="body" ref={bodyInputRef} />
+        </label>
+        <br />
+        <label htmlFor="image">
+          Image:
+          <input type="img" id="image" ref={imageInputRef} />
+        </label>
+        <br />
+        <label htmlFor="source">
+          Source:
+          <input type="text" id="source" ref={sourceInputRef} />
+        </label>
+        <br />
+        <label htmlFor="publisher">
+          Publisher:
+          <input type="text" id="publisher" ref={publisherInputRef} />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+        <button type="submit" onClick={handleCloseModal}>
+          Cancel
+        </button>
+      </form>
     </div>
-  )
-
-};
+  );
+}
 
 export default FeedDetail;
--- src/components/FeedItem.js
+++ src/components/FeedItem.js
@@ -1,16 +1,41 @@
 /*Modelo Feed que tenga los atributos: title, body, image, source y publisher */
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+import FeedDetail from './FeedDetail';
 import './FeedItem.css'
 
 const FeedItem = (props) => {
-  const { title, content, urlToImage, source, author } = props.feed
+
+  
+  const [feed, setFeed] = useState(props.feed);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const handleOpenModal = (feed) => {
+    setModalIsOpen(true);
+  };
+
+  const editNew = (feedBody) => {
+    setFeed(feedBody)
+    console.log(feedBody)
+  }
+
+  const handleCloseModal = () => {
+    setModalIsOpen(false);
+  };
+
   return (
     <div className='feed-item'>
-      <h2>Title: {title}</h2>
-      <p className='body'>{content}</p>
-      <img src={urlToImage} alt={title} />
-      <p className='source'>{source.name}</p>
-      <p className='publisher'>{author?? "Anonymous"}</p>
+      <h2>Title: {feed.title}</h2>
+      <p className='body'>{feed.content}</p>
+      <img src={feed.urlToImage} alt={feed.title} />
+      <p className='source'>{feed.source.name}</p>
+      <p className='publisher'>{feed.author?? "Anonymous"}</p>
+      <button onClick={() => handleOpenModal()}>
+        Edit
+      </button>
+      <Modal className='feed-detail-modal' isOpen={modalIsOpen} >
+        <FeedDetail editNew={(feedBody) => editNew(feedBody)} handleCloseModal={handleCloseModal}/>
+      </Modal>
     </div>
   );
 };
